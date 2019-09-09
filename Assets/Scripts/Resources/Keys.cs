31339e21103229c68d85f3e698eb9edf4a3e3272
Unknown <aoviedo@pixframestudios.com>
2019-09-09 11:44:34 -0500

    Add unity ads needed for ios version

--- Assets/Scripts/Resources/Keys.cs
+++ Assets/Scripts/Resources/Keys.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class Keys : MonoBehaviour {
 
-    //public static string Api_Web_Key = "https://towi.nyx.mx:2443/";
+    public static string Api_Web_Key = "https://towi.nyx.mx:2443/";
     //public static string Api_Web_Key = "http://35.232.245.131/";
-    public static string Api_Web_Key = "https://towi-testing.nyx.mx/";
+    //public static string Api_Web_Key = "https://towi-testing.nyx.mx/";
     public static string Try_Connection_Key = "api/levels/connection/";
     public static string First_Try = "frstTry";
     public static string Level_Of_Try = "lvlFTry";
