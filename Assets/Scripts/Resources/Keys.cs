d0f8bb50a5e373bebb3e17b917ef8fa02fab137f
Raúl Sánchez <shigelorsh@gmail.com>
2023-04-14 15:32:09 -0600

    conexion

--- Assets/Scripts/Resources/Keys.cs
+++ Assets/Scripts/Resources/Keys.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class Keys
 {
 
-    public static string Api_Web_Key = "https://towi.nyx.mx:2443/";
-    //public static string Api_Web_Key = "http://35.232.245.131/";
+    //public static string Api_Web_Key = "https://towi.nyx.mx:2443/";
+    public static string Api_Web_Key = "http://54.243.247.92:2443/";
     //public static string Api_Web_Key = "https://towi-testing.nyx.mx/";
     public static string Try_Connection_Key = "api/levels/connection/";
     public static string First_Try = "frstTry";
